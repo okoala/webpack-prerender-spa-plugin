bb2900f91b10ce1793eb801aa7a5fc4be80520c4
okoala <dapixp@gmail.com>
2016-10-20 00:27:03 +0800

    Fix run server error

--- index.js
+++ index.js
@@ -14,33 +14,36 @@ function HtmlPrecompiler (options) {
 
 HtmlPrecompiler.prototype.apply = function (compiler) {
   var self = this
-  var server = childprocess.fork(self.entryFile, {env: {NODE_ENV: process.env.NODE_ENV || 'production'}})
-  var successCount = 0
-
-  server.on('error', err => {
-    console.error(err)
-  })
-
   compiler.plugin('emit', function (compilation, done) {
-    self.routes.forEach(function (route) {
-      compileToHTML(self.entryFile, self.serverUrl, route, self.options, function (prerenderedHTML) {
-        var folder = path.join(self.dist, route)
-        mkdirp(folder, function (error) {
-          if (error) throw error
-          fs.writeFile(
-            path.join(folder, 'index.html'),
-            prerenderedHTML,
-            function (error) {
-              if (error) throw error
-              successCount++
-              if (successCount === self.routes.length) {
-                server.kill()
+    var server = childprocess.fork(self.entryFile, {env: {NODE_ENV: process.env.NODE_ENV || 'production'}})
+    var successCount = 0
+
+    server.on('error', err => {
+      console.error(err)
+    })
+
+    setTimeout(function() {
+      self.routes.forEach(function (route) {
+        compileToHTML(self.entryFile, self.serverUrl, route, self.options, function (prerenderedHTML) {
+          var folder = path.join(self.dist, route)
+          mkdirp(folder, function (error) {
+            if (error) throw error
+            fs.writeFile(
+              path.join(folder, 'index.html'),
+              prerenderedHTML,
+              function (error) {
+                if (error) throw error
+                successCount++
+                if (successCount === self.routes.length) {
+                  server.kill()
+                }
               }
-            }
-          )
+            )
+          })
         })
       })
-    })
+    }, 10000)
+
     done()
   })
 }
